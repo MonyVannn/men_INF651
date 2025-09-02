ceb5e0e770560c69bd622d3cb8176eff11b5ddba
Monyvann <m.men6603@gmail.com>
2025-09-02 16:12:57 -0500

    Add Assignment 2

---
+++ assignments/assignment2/main.js
@@ -0,0 +1,66 @@
+// 1. Variables and Data Types
+let student = "Monyvann";
+let age = 22;
+let isStudent = true;
+
+console.log("Student name: ", student, " has a type of ", typeof student);
+console.log("Student age: ", age, " has a type of ", typeof age);
+console.log(
+  "Whether they are a student?: ",
+  isStudent,
+  " has a type of ",
+  typeof isStudent
+);
+
+// 2. Basic Arithmetic Operations
+let num1 = 10;
+let num2 = 20;
+
+addition = 10 + 20;
+console.log("10 + 20 = ", addition);
+
+substraction = 10 - 20;
+console.log("10 - 20 = ", substraction);
+
+multiplication = 10 * 20;
+console.log("10 * 20 = ", multiplication);
+
+division = 10 / 20;
+console.log("10 / 20 = ", division);
+
+// 3. Working with Strings
+let str = "I am very lazy";
+console.log("The length of this sentence is: ", str.length);
+console.log("The first letter of this sentence is: ", str[0]);
+console.log("The last letter of this sentence is: ", str[str.length - 1]);
+
+// 4. Math Object
+let negNum = -4;
+console.log("The squared root of ", negNum, " is: ", Math.sqrt(negNum));
+console.log("The number squared of ", negNum, " is: ", Math.pow(negNum, 2));
+console.log("The absolute value of ", negNum, " is: ", Math.abs(negNum));
+
+// 5. Boolean Logic and Comparison Operators
+let a = 10;
+let b = 20;
+if (a > b) {
+  console.log(a + " is greater than " + b);
+} else if (a < b) {
+  console.log(a + " is less than " + b);
+} else {
+  console.log(a + " is equal to " + b);
+}
+
+// 6. Logical Operators
+let bool1 = true;
+let bool2 = false;
+console.log("bool1 AND bool2: ", bool1 && bool2);
+console.log("bool1 OR bool2: ", bool1 || bool2);
+console.log("NOT bool1: ", !bool1);
+console.log("NOT bool2: ", !bool2);
+
+// 7. Using Template Literals
+let firstName = "Mony";
+let lastName = "Vann";
+let greetingMessage = `Hello, my name is ${firstName} ${lastName}.`;
+console.log(greetingMessage);
